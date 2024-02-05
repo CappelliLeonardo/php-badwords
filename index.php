76cc1f52456ed82f5ac26c3410d403393c40b271
CappelliLeonardo <l.cappelli1995@gmail.com>
2024-02-05 15:01:30 +0100

    Add textarea

---
+++ index.php
@@ -0,0 +1,29 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body>
+
+<form action="./secondFile.php" method="GET">
+
+    <div>
+
+        <textarea name="inputUser" id="" cols="30" rows="10">
+
+        </textarea>
+    </div>
+
+
+    <button>
+        Invia
+    </button>
+
+</form>
+    
+</body>
+</html>
